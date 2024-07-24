75b0877cfbddcf9a4d0c6c8745e85177b21755c2
EleyOliveira <eleyoliveira@bol.com.br>
2024-07-24 07:15:51 -0300

    incluido handler

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "net/http"
+
 func main() {
+	http.HandleFunc("/", cepHandler)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func cepHandler(w http.ResponseWriter, r *http.Request) {
 
 }
