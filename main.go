9ca7d2040abf7d049e07c31680454a37abe7822a
EleyOliveira <eleyoliveira@bol.com.br>
2024-07-28 18:47:42 -0300

    incluido parâmetro cep

--- main.go
+++ main.go
@@ -6,13 +6,22 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", cepHandler)
+	http.HandleFunc("/", BuscaCepHandler)
 	http.ListenAndServe(":8080", nil)
 
 }
 
-func cepHandler(w http.ResponseWriter, r *http.Request) {
-	req, err := http.Get("http://viacep.com.br/ws/06182030/json/")
+func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	cepBusca := r.URL.Query().Get("cep")
+	if cepBusca == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	req, err := http.Get("http://viacep.com.br/ws/" + cepBusca + "/json/")
 	if err != nil {
 		panic(err)
 	}
@@ -22,7 +31,7 @@ func cepHandler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	w.Write(res)
-	req, err = http.Get("https://brasilapi.com.br/api/cep/v1/62051032")
+	req, err = http.Get("https://brasilapi.com.br/api/cep/v1/" + cepBusca)
 	if err != nil {
 		panic(err)
 	}
