8e2b16c45e9c76b1d7c03b8d7b890a2c0088bf9b
EleyOliveira <eleyoliveira@bol.com.br>
2024-07-28 18:32:37 -0300

    requisição para os endpoints

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", cepHandler)
@@ -9,5 +12,24 @@ func main() {
 }
 
 func cepHandler(w http.ResponseWriter, r *http.Request) {
-
+	req, err := http.Get("http://viacep.com.br/ws/06182030/json/")
+	if err != nil {
+		panic(err)
+	}
+	defer req.Body.Close()
+	res, err := io.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	w.Write(res)
+	req, err = http.Get("https://brasilapi.com.br/api/cep/v1/62051032")
+	if err != nil {
+		panic(err)
+	}
+	defer req.Body.Close()
+	res, err = io.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	w.Write(res)
 }
