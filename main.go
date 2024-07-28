32e0fade3072628c476aa24d6d865fd372201273
EleyOliveira <eleyoliveira@bol.com.br>
2024-07-28 20:03:56 -0300

    exibição do log

--- main.go
+++ main.go
@@ -2,43 +2,38 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", BuscaCepHandler)
-	http.ListenAndServe(":8080", nil)
+	cepBusca := "06182030"
 
-}
+	time.Sleep(10 * time.Second)
+	func() {
+		req, err := http.Get("http://viacep.com.br/ws/" + cepBusca + "/json/")
+		if err != nil {
+			panic(err)
+		}
+		defer req.Body.Close()
+		res, err := io.ReadAll(req.Body)
+		if err != nil {
+			panic(err)
+		}
+		log.Println(string(res))
+	}()
 
-func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	cepBusca := r.URL.Query().Get("cep")
-	if cepBusca == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	req, err := http.Get("http://viacep.com.br/ws/" + cepBusca + "/json/")
-	if err != nil {
-		panic(err)
-	}
-	defer req.Body.Close()
-	res, err := io.ReadAll(req.Body)
-	if err != nil {
-		panic(err)
-	}
-	w.Write(res)
-	req, err = http.Get("https://brasilapi.com.br/api/cep/v1/" + cepBusca)
-	if err != nil {
-		panic(err)
-	}
-	defer req.Body.Close()
-	res, err = io.ReadAll(req.Body)
-	if err != nil {
-		panic(err)
-	}
-	w.Write(res)
+	func() {
+		req, err := http.Get("https://brasilapi.com.br/api/cep/v1/" + cepBusca)
+		if err != nil {
+			panic(err)
+		}
+		defer req.Body.Close()
+		res, err := io.ReadAll(req.Body)
+		if err != nil {
+			panic(err)
+		}
+		log.Println(string(res))
+	}()
 }
